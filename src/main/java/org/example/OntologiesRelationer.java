9132875410b5a492cd9fa76e52a1193fa6e391c6
Daniel Naro <dnaro@ac.upc.edu>
2025-06-03 08:10:32 +0200

    Adding relationer with Spyderisk

---
+++ src/main/java/org/example/OntologiesRelationer.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.semanticweb.owlapi.model.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class OntologiesRelationer {
+    public static OWLOntology addNewTypesToNamedIndividuals(OWLOntology ontology, OWLOntologyManager manager, Map<List<String>, List<String>> additions) {
+        OWLDataFactory factory = manager.getOWLDataFactory();
+        for (Map.Entry<List<String>, List<String>> entry : additions.entrySet()) {
+            for (String namedIndividualURI : entry.getKey()) {
+                for (String newClassURI : entry.getValue()) {;
+                    OWLClass newClass = factory.getOWLClass(newClassURI);
+
+                    IRI individualIRI = IRI.create(namedIndividualURI);
+                    OWLNamedIndividual individual = factory.getOWLNamedIndividual(individualIRI);
+
+                    OWLClassAssertionAxiom classAssertion = factory.getOWLClassAssertionAxiom(newClass, individual);
+
+                    manager.addAxiom(ontology, classAssertion);
+                }
+
+
+            }
+        }
+        return ontology;
+    }
+
+    public static OWLOntology addEquivalences(OWLOntology ontology, OWLOntologyManager manager, Map<String, String> equivalences) {
+        OWLDataFactory factory = manager.getOWLDataFactory();
+        for (Map.Entry<String, String> entry : equivalences.entrySet()) {
+            OWLClass classA = factory.getOWLClass(IRI.create(entry.getKey()));
+            OWLClass classB = factory.getOWLClass(IRI.create(entry.getValue()));
+
+            OWLEquivalentClassesAxiom eqAxiom = factory.getOWLEquivalentClassesAxiom(classA, classB);
+
+            manager.addAxiom(ontology, eqAxiom);
+        }
+        return ontology;
+    }
+
+    public static OWLOntology addImport(OWLOntology ontology, OWLOntologyManager manager, String importedUrl){
+        IRI importOntologyIRI = IRI.create(importedUrl); // Modify with actual IRI
+
+        OWLImportsDeclaration importDeclaration = manager.getOWLDataFactory().getOWLImportsDeclaration(importOntologyIRI);
+
+        manager.applyChange(new AddImport(ontology, importDeclaration));
+
+        return ontology;
+    }
+}
